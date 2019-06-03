e397e079a9c0240e0a7627959e763b8d1e3d9150
Bright Ket-udom <supawitket@outlook.com>
2019-06-04 01:13:43 +0700

    Init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=fy20helpdeskbot-Web-Deploy.pubxml -p:Password=CyxkeKhYyngcazu88thiF5FGYgXb2seFLvCtLYuqiTdpwgqNc8Pk65Pf54jq
+
