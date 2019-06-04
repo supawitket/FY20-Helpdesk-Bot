ae647de87094b980d852127ca131cdadcfcd8f69
Bright Ket-udom <supawitket@outlook.com>
2019-06-04 09:07:59 +0700

    init

--- bot.js
+++ bot.js
@@ -9,7 +9,8 @@ class EchoBot extends ActivityHandler {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
             if(context.activity.text.indexOf('ดี') !== -1 ) {
-                await context.sendActivity("สวัสดีค่ะ");
+                // await context.sendActivity("สวัสดีค่ะ");
+                await stepContext.context.sendActivity({ attachments: [this.createHeroCard()] });
             } else {
                 await context.sendActivity("hello");
             }
@@ -23,6 +24,7 @@ class EchoBot extends ActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             // await next();
         });
+        
 
         // this.onMembersAdded(async (context, next) => {
         //     const membersAdded = context.activity.membersAdded;
@@ -34,6 +36,21 @@ class EchoBot extends ActivityHandler {
         //     // By calling next() you ensure that the next BotHandler is run.
         //     await next();
         // });
+
+        
+    }
+    createHeroCard() {
+        return CardFactory.heroCard(
+            'BotFramework Hero Card',
+            CardFactory.images(['https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg']),
+            CardFactory.actions([
+                {
+                    type: 'openUrl',
+                    title: 'Get started',
+                    value: 'https://docs.microsoft.com/en-us/azure/bot-service/'
+                }
+            ])
+        );
     }
 }
 
