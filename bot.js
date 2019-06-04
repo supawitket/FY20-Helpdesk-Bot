06ece8a7eed6e7660865252e732b2ba03b8e1b3e
Bright Ket-udom <supawitket@outlook.com>
2019-06-04 08:46:17 +0700

    init

--- bot.js
+++ bot.js
@@ -19,16 +19,16 @@ class EchoBot extends ActivityHandler {
             await next();
         });
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome!');
-                }
-            }
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
+        // this.onMembersAdded(async (context, next) => {
+        //     const membersAdded = context.activity.membersAdded;
+        //     for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+        //         if (membersAdded[cnt].id !== context.activity.recipient.id) {
+        //             await context.sendActivity('Hello and welcome!');
+        //         }
+        //     }
+        //     // By calling next() you ensure that the next BotHandler is run.
+        //     await next();
+        // });
     }
 }
 
